aef61fea62373411ff01076a8641b5ca13acdd37
RutviKeshwani <rkeshwan@students.kennesaw.edu>
2025-07-23 23:57:07 -0400

    Update update_schema.py
    Added a age, bio, Interests columns in users table to store users information under their profile page

--- update_schema.py
+++ update_schema.py
@@ -2,7 +2,7 @@ import sqlite3
 
 DB_NAME = 'agelink.db'
 
-with sqlite3.connect(DB_NAME) as conn:
+with sqlite3.connect(DB_NAME) as conn: 
     c = conn.cursor()
     c.execute("ALTER TABLE users ADD COLUMN age INTEGER")
     c.execute("ALTER TABLE users ADD COLUMN bio TEXT")
